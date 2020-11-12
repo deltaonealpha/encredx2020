7f186111da60548a2e57cc1e16aeee63e9ab4c90
Pranav Balaji <balaji.pranav@outlook.in>
2020-11-12 16:55:34 +0530

    xddx

---
+++ dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "remotemysql.com:3306";
+$dbUsername = "JOVo7uPSRu";
+$dbPassword = "ozBCJkwyfp";
+$dbName = "JOVo7uPSRu";
+
+$conn = mysqli_connect($dbServername, $dbUsername , $dbPassword, $dbName);

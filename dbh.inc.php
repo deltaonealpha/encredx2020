cca6f4d3dd8649b57f475a6993d6ce51e2418bf0
Pranav Balaji <balaji.pranav@outlook.in>
2020-11-12 20:09:00 +0530

    Added home, login, register

--- dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "remotemysql.com:3306";
-$dbUsername = "JOVo7uPSRu";
-$dbPassword = "ozBCJkwyfp";
-$dbName = "JOVo7uPSRu";
-
-$conn = mysqli_connect($dbServername, $dbUsername , $dbPassword, $dbName);
